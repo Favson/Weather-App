b31f1a6d6950da6c49f0186e10b10f6eb781e9c0
Adebisi <adebisimomoluwa@gmail.com>
2024-02-05 23:40:23 +0100

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,13 @@ const weatherDetails = document.querySelector('.weather-details');
 // const container = document.querySelector(".container");
 // const container = document.querySelector(.container);
 
-search.addEventListener{'click', () =>{
+search.addEventListener('click', () =>{
 
-    const APIKEY ='012fd6ecfa215db82b32c682e4c1cfdd'
+    const APIKEY ='012fd6ecfa215db82b32c682e4c1cfdd';
+    const city = document.querySelector('.search-box input').value;
 
-}};
+    if(city == ""){
+        return;
+
+    fetch(``)
+});
