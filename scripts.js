84b2ec69524595580c24d4348920f3c5c75f4fad
Adebisi <adebisimomoluwa@gmail.com>
2024-02-06 15:11:38 +0100

    delete 1 file and update 3 files

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector('.weather-details');
-// const container = document.querySelector(".container");
-// const container = document.querySelector(.container);
+const error404 = document.querySelector(".not-found");
+const cityHide = document.querySelector('.city-hide');
 
 search.addEventListener('click', () =>{
     const APIKEY ='012fd6ecfa215db82b32c682e4c1cfdd';
@@ -14,35 +14,59 @@ search.addEventListener('click', () =>{
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`).then(Response => Response.json()).then(json=> {
 
+    if(json.cod == 404){
+        cityHide.textContent= city;
+        container.style.height='400px'; 
+        weatherBox.classList.remove('active');
+        weatherDetails.classList.remove('active');
+        error404.classList.add('active');
+        // error404.classList.add('active')
+        return;
+    }
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity span');
         const wind = document.querySelector('.weather-details .wind span');
-        // const image = document.querySelector('.weather-details img')
 
-        switch (json.weather[0].main) {
-            case 'Clear':
-                image.src='images/clear sky.png'
-                break;
+        if(cityHide.textContent == city){
+            return;
+        }else{
+            cityHide.textContent = city;
 
-            case 'Rain':
-                image.src='images/rain.png'
-                break;
-            case 'Snow':
-                image.src='images/snow.png'
-                break;
-            case 'clouds':
-                image.src='images/clouds.png'
-                break;
-            case 'Mist':
-                image.src='images/mist.png'
-                break;
-            case 'Haze':
-                image.src='images/haze.png'
-                break;
-            default:
-                image.src='images/not-found.png'
+            container.style.height='555px' 
+            container.classList.add('active')
+            weatherBox.classList.add('active')
+            weatherDetails.classList.add('active')
+            error404.classList.remove('active')
+
+            setTimeout(() => {
+            container.classList.remove('active')
+            }, 2500);
+
+            if(json.weather[0].main == 'Clouds') {
+                image.src='Images/clouds.png'
+            }else if (json.weather[0].main == 'Clear'){
+                image.src='Images/clear sky.png'
+            }else if (json.weather[0].main == 'Rain'){
+                image.src='Images/rain.png'
+            }else if (json.weather[0].main == 'Snow'){
+                image.src='Images/snow.png'
+            }else if (json.weather[0].main == 'Mists'){
+                image.src='Images/mist.png'
+            }else if (json.weather[0].main == 'Haze'){
+                image.src='Images/haze.png'
+            }else if(json.weather[0].main == ''){
+                image.src = 'Images/not-found.png'
+            }else{
+                image.src='Images/default.png'
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
+            description.innerHTML = `${json.weather[0].description}`
+            humidity.innerHTML =`${json.main.humidity}%`
+            wind.innerHTML=`${parseInt(json.wind.speed)}Km/h`
         }
+
     });
 });
